4c5a45132e49c2f7d28f8078b42e51a69da252bd
Nate Vack <njvack@wisc.edu>
2008-05-23 19:40:32 +0000

    Specced, added rparsec-based calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,49 @@
+# Part of the Optimus package for managing E-Prime data
+# 
+# Copyright (C) 2008 Board of Regents of the University of Wisconsin System
+# 
+# Written by Nathan Vack <njvack@wisc.edu>, at the Waisman Laborotory for Brain
+# Imaging and Behavior, University of Wisconsin - Madison
+#
+# Calculator code adapted from rparsec infix calculator demo
+# at http://docs.codehaus.org/display/JPARSEC/Ruby+Parsec
+# rparsec (C) Ben Yu
+
+require 'rubygems'
+require 'rparsec'
+include RParsec
+
+module Eprime
+  class Calculator
+    include Parsers
+    include Functors
+    
+    Mod = lambda { |x, y| x.to_i % y.to_i }
+    
+    def initialize
+      ops = OperatorTable.new.
+        infixl(char(?+) >> Plus, 20).
+        infixl(char(?-) >> Minus, 20).
+        infixl(char(?*) >> Mul, 40).
+        infixl(char(?/) >> Div, 40).
+        infixl(char(?%) >> Mod, 40).
+        prefix(char(?-) >> Neg, 60)
+      expr = nil
+      #int_parser = integer.map(&To_i) 
+      float_parser = number.map(&To_f)
+      grouping_parser = char('(') >>(lazy{expr}) << char(')')
+      term = alt(float_parser, grouping_parser)
+      delim = whitespace.many_
+      expr = delim >> Expressions.build(term, ops, delim)
+      @parser = expr
+    end
+    
+    def compute(expression)
+      ans = @parser.parse(expression)
+      if (ans - ans.to_i) == 0
+        ans = ans.to_i
+      end
+      return ans.to_s
+    end
+  end
+end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,51 @@
+# Part of the Optimus package for managing E-Prime data
+# 
+# Copyright (C) 2008 Board of Regents of the University of Wisconsin System
+# 
+# Written by Nathan Vack <njvack@wisc.edu>, at the Waisman Laborotory for Brain
+# Imaging and Behavior, University of Wisconsin - Madison
+
+require File.join(File.dirname(__FILE__),'spec_helper')
+require File.join(File.dirname(__FILE__), '../lib/eprime')
+require 'calculator'
+
+include EprimeTestHelper
+
+describe Eprime::Calculator do
+  before :all do
+    @calc = Eprime::Calculator.new
+    @x = EXPRS
+  end
+  
+  it "should compute constants" do
+    @calc.compute(es(:const)).should == ev(:const)
+  end
+  
+  it "should add" do
+    @calc.compute(es(:add)).should == ev(:add)
+  end
+  
+  it "should multiply" do
+    @calc.compute(es(:mul)).should == ev(:mul)
+  end
+  
+  it "should handle negation" do
+    @calc.compute(es(:add_neg)).should == ev(:add_neg)
+  end
+  
+  it "should handle grouping" do
+    @calc.compute(es(:add_mul_group)).should == ev(:add_mul_group)
+  end
+  
+  it "should handle fdiv" do
+    @calc.compute(es(:fdiv)).should == ev(:fdiv)
+  end
+  
+  it "should handle fmul" do
+    @calc.compute(es(:fmul)).should == ev(:fmul)
+  end
+  
+  it "should handle mod" do
+    @calc.compute(es(:mod)).should == ev(:mod)
+  end
+end
