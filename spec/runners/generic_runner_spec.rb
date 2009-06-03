803229fa941ecd0ae7ac6edcb52f6d9736eaf630
Nate Vack <njvack@wisc.edu>
2009-06-03 18:04:43 +0000

    Fixed comment preamble in spec

--- spec/runners/generic_runner_spec.rb
+++ spec/runners/generic_runner_spec.rb
@@ -5,13 +5,6 @@
 # Written by Nathan Vack <njvack@wisc.edu>, at the Waisman Laborotory for Brain
 # Imaging and Behavior, University of Wisconsin - Madison
 
-# Part of the Optimus package for managing E-Prime data
-# 
-# Copyright (C) 2008-09 Board of Regents of the University of Wisconsin System
-# 
-# Written by Nathan Vack <njvack@wisc.edu>, at the Waisman Laborotory for Brain
-# Imaging and Behavior, University of Wisconsin - Madison
-
 require File.join(File.dirname(__FILE__),'../spec_helper')
 require File.join(File.dirname(__FILE__), '../../lib/eprime')
 require 'runners/generic_runner'
